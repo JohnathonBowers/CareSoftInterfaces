9358a5eafe88489dcfc8df1b7427f424c98e5012
Johnathon Bowers <johnathonbowers@gmail.com>
2022-12-11 00:50:27 -0500

    complete code

--- src/com/caresoft/clinicapp/AdminUser.java
+++ src/com/caresoft/clinicapp/AdminUser.java
@@ -1,5 +1,80 @@
 package com.caresoft.clinicapp;
 
+import java.util.Date;
+
+import java.util.ArrayList;
+
 public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
 
+	private String role;
+	private ArrayList<String> securityIncidents = new ArrayList<String>();
+	
+	// TO DO: Implement a constructor that takes an ID and a role
+	
+	public AdminUser (Integer id, String role) {
+		super();
+		this.id = id;
+		this.role = role;
+	}
+
+	
+	// Class Methods
+	
+	public void newIncident(String notes) {
+		String report = String.format("\n\nDatetime Submitted: %s\nReported By ID: %s\nNotes: %s", new Date(), this.id, notes);
+		securityIncidents.add(report);
+	}
+
+	public void authIncident() {
+		String report = String.format("\n\nDatetime Submitted: %s\nID: %s\nNotes: %s", new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER");
+		securityIncidents.add(report);
+	}
+	
+	
+	// TO DO: Implement HIPAA CompliantUser!
+	
+	@Override
+	public boolean assignPin(int pin) {
+		if (pin > 99999 && pin < 1000000) {
+			setPin(pin);
+			return true;
+		} else {
+		return false;
+		}
+	}
+
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if (id == confirmedAuthID) {
+			return true;
+		} else {
+			authIncident();
+			return false;
+		}
+	}
+	
+	@Override
+	public ArrayList<String> reportSecurityIncidents() {
+		return securityIncidents;
+	}
+	
+	
+	// TO DO: Getters & Setters
+	
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public ArrayList<String> getSecurityIncidents() {
+		return securityIncidents;
+	}
+
+	public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+		this.securityIncidents = securityIncidents;
+	}
+	
 }
--- src/com/caresoft/clinicapp/Physician.java
+++ src/com/caresoft/clinicapp/Physician.java
@@ -1,5 +1,57 @@
 package com.caresoft.clinicapp;
 
+import java.util.Date;
+
+import java.util.ArrayList;
+
 public class Physician extends User implements HIPAACompliantUser {
 
+	private ArrayList<String> patientNotes = new ArrayList<String>();
+	
+	// TO DO: Constructor that takes an ID
+	
+	public Physician (Integer id) {
+		super();
+		this.id = id;
+	}
+
+	// TO DO: Implement HIPAACompliantUser!
+	
+	@Override
+	public boolean assignPin(int pin) {
+		if (pin > 999 && pin < 10000) {
+			setPin(pin);
+			return true;
+		} else {
+		return false;
+		}
+	}
+	
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if (id == confirmedAuthID) {
+			return true;
+		} else {
+		return false;
+		}
+	}
+	
+	public void newPatientNotes(String notes, String patientName, Date date) {
+		String report = String.format("Datetime Submitted: %s\n", date);
+		report += String.format("Reported By ID: %s", this.id);
+		report += String.format("Patient Name: %s\n", patientName);
+		report += String.format("Notes: %s\n", notes);
+		this.patientNotes.add(report);
+	}
+	
+	// TO DO: Getters & Setters
+	
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
+
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
+	
 }
